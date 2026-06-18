acaaba598394a0d7e252b5eb84db80635ef1c075
FPatato <louca.cc26@gmail.com>
2026-06-18 19:03:48 -0400

    fuck off

--- scripts/videos.js
+++ scripts/videos.js
@@ -1158,7 +1158,7 @@
       languages: ["FR"],
       seasons: RICK_AND_MORTY_SEASONS
     }),
-    buildSeries({
+    buildSeries({//test
       id: "supernatural",
       title: "Supernatural",
       date: "2005",
